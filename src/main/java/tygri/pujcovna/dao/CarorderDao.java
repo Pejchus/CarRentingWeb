92e4d75b4d5a4630890b2f86083aaa27f5896d5e
Petr <krouppe2@fel.cvut.cz>
2020-04-25 23:43:53 +0200

    Car order filter done

--- src/main/java/tygri/pujcovna/dao/CarorderDao.java
+++ src/main/java/tygri/pujcovna/dao/CarorderDao.java
@@ -1,6 +1,7 @@
 package tygri.pujcovna.dao;
 
 import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 import tygri.pujcovna.model.Car;
@@ -18,8 +19,34 @@ public class CarorderDao extends BaseDao {
         return super.getAll();
     }
 
+    /**
+     * Every order is created with 0's in time part of the begin and end
+     * Timestamp
+     *
+     * @param customer
+     * @param begin
+     * @param end
+     * @param car
+     * @param price
+     * @param paid
+     * @return
+     */
     public boolean createCarorder(User customer, Timestamp begin, Timestamp end, Car car, double price, boolean paid) {
         try {
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(begin.getTime());
+            c.set(Calendar.HOUR_OF_DAY, 0);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.SECOND, 0);
+            c.set(Calendar.MILLISECOND, 0);
+            begin.setTime(c.getTimeInMillis());
+            c = Calendar.getInstance();
+            c.setTimeInMillis(end.getTime());
+            c.set(Calendar.HOUR_OF_DAY, 0);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.SECOND, 0);
+            c.set(Calendar.MILLISECOND, 0);
+            end.setTime(c.getTimeInMillis());
             Carorder order = new Carorder(customer, car, begin, end, new Timestamp(System.currentTimeMillis()), price, paid);
             persist(order);
         } catch (Exception e) {
@@ -70,7 +97,7 @@ public class CarorderDao extends BaseDao {
      */
     public List<Carorder> getReservations(Car car, Timestamp startDate, Timestamp endDate) {
         try {
-            return em.createQuery("SELECT e FROM Carorder e where e.car=:car and (e.enddate>:startDate or e.begindate<:endDate)").setParameter("startDate", startDate).setParameter("endDate", endDate).setParameter("car", car).getResultList();
+            return em.createQuery("SELECT e FROM Carorder e WHERE e.car=:car AND NOT (e.begindate>:endDate OR e.enddate<:startDate)").setParameter("startDate", startDate).setParameter("endDate", endDate).setParameter("car", car).getResultList();
         } catch (Exception e) {
             return null;
         }
@@ -85,7 +112,7 @@ public class CarorderDao extends BaseDao {
      */
     public List<Carorder> getReservationsFrom(Car car, Timestamp startDate) {
         try {
-            return em.createQuery("SELECT e FROM Carorder e where e.car=:car and e.enddate>:startDate").setParameter("startDate", startDate).setParameter("car", car).getResultList();
+            return em.createQuery("SELECT e FROM Carorder e where e.car=:car and e.enddate>=:startDate").setParameter("startDate", startDate).setParameter("car", car).getResultList();
         } catch (Exception e) {
             return null;
         }
@@ -100,7 +127,7 @@ public class CarorderDao extends BaseDao {
      */
     public List<Carorder> getReservationsUpTo(Car car, Timestamp endDate) {
         try {
-            return em.createQuery("SELECT e FROM Carorder e where e.car=:car and e.begindate<:endDate").setParameter("endDate", endDate).setParameter("car", car).getResultList();
+            return em.createQuery("SELECT e FROM Carorder e where e.car=:car and e.begindate<=:endDate").setParameter("endDate", endDate).setParameter("car", car).getResultList();
         } catch (Exception e) {
             return null;
         }
