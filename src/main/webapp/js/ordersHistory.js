229e8ee9d1f11db17f7fbc17a76f037484025b14
Michal <kolarm32@fel.cvut.cz>
2020-04-29 00:36:58 +0200

    carProfile reservations, disable cancel option for past reservations

--- src/main/webapp/js/ordersHistory.js
+++ src/main/webapp/js/ordersHistory.js
@@ -1,13 +1,21 @@
 var tableData = $("#ordersHistoryTable").find('tbody tr td');
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
-for (var i = 0; i <= tableData.length; i += 6) {
-    var date = tableData[i];
-    var beginDate = tableData[i + 2];
-    var endDate = tableData[i + 3];
-    var tmp = beginDate.innerHTML.split(" ");
+for (var i = 0; i < tableData.length; i += 6) {
+    var createdDate = tableData[i],
+     beginDate = tableData[i + 2],
+     endDate = tableData[i + 3],
+     tmp = beginDate.innerHTML.split(" "),
+     beginDateD = new Date(Date.parse(tmp));
+
     tableData[i + 2].innerHTML = tmp[0];
     tmp = endDate.innerHTML.split(" ");
     tableData[i + 3].innerHTML = tmp[0];
-    tmp = date.innerHTML.split(" ");
+    tmp = createdDate.innerHTML.split(" ");
     tableData[i].innerHTML = tmp[0];
+
+    if (beginDateD <= Date.parse(date)) {
+        tableData[i + 5].innerHTML = "";
+    }
 }
