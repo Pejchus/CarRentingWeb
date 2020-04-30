9c02994441816074f281aa9d04bf1552e7d37a34
Jakub Vaník <vanikja2@fel.cvut.cz>
2020-04-30 11:09:20 +0200

    rychla oprava

--- src/main/java/tygri/pujcovna/controllers/CarProfileController.java
+++ src/main/java/tygri/pujcovna/controllers/CarProfileController.java
@@ -209,7 +209,6 @@ public class CarProfileController {
     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_CUSTOMER')")
     @RequestMapping(value = "/deleteCarOrder", method = RequestMethod.GET)
     public ModelAndView deleteCarOrder(HttpSession session, @RequestParam("id") String id) {
-        ModelAndView mv = new ModelAndView("profile.jsp");
         String userId= session.getAttribute("userId").toString();
         String userIdOwner=carorderService.getCarOrderOwner(id);
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
